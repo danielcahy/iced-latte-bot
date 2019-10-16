5c0990013eb845e17b224edf1d8850491b6e2a5d
Daniel Cahyo <d4niel.cahyo@gmail.com>
2019-10-16 23:53:09 +0800

    stroke.js is now using async/await

--- commands/stroke.js
+++ commands/stroke.js
@@ -6,33 +6,41 @@ let cheerio = require('cheerio')
 let urlencode = require('urlencode')
 
 
-let strokeOrder = (message) => {
+async function strokeOrder(message) {
     // Get character from second array element of splitted string
     let character = message.content.split(' ')[1]
     // Encode character to URL readable
     let encodedCharacter = urlencode(character)
     // Set URL
     let url = `http://www.strokeorder.info/mandarin.php?q=${encodedCharacter}`
-    // Options setup
-    let options = {
-        url: url,
-        headers: {
-            'User-Agent': 'request'
+    // Create request promise
+    let requestPromise = new Promise((resolve, reject) => {
+        // Options setup
+        let options = {
+            url: url,
+            headers: {
+                'User-Agent': 'request'
+            }
         }
+        // Callback setup
+        let callback = (err, res, body) => {
+            if (res.statusCode === 200) {
+                // Load body to cheerio
+                const $ = cheerio.load(body)
+                // Get .gif URL
+                let imageSource = $('h1').next().attr('src')
+                resolve(imageSource)                
+            } else {
+                resolve('Character doesn\'t exist')
+            }
 
-    }
-    // Callback setup
-    let callback = (err, res, body) => {
-        // Load body to cheerio
-        const $ = cheerio.load(body)
-        // Get .gif URL
-        let imgURL = $('h1').next().attr('src')
-        // Send stroke order .gif
-        message.channel.send(imgURL)
-    }
+        }
+        request(options, callback)
+    })
     // Run request
-    request(options, callback)
-    
+    let result = await requestPromise
+    // Send stroke order .gif
+    message.channel.send(result)         
 }
 
 module.exports = strokeOrder
