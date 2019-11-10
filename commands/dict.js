fa88d2a0e2453f2297206f140ccbdd7d175b82d0
Daniel Cahyo <d4niel.cahyo@gmail.com>
2019-11-10 21:05:52 +0800

    :05

--- commands/dict.js
+++ commands/dict.js
@@ -3,24 +3,24 @@ let dict = (message) => {
 
     message.channel.send('Type something..')
     .then(msg => {
-        const filter = m => m.author === message.author
-        const collector = message.channel.createMessageCollector(filter, { time: 15000 });
+        const filter = m => m.author === msg.author
+        const collector = msg.channel.createMessageCollector(filter, { time: 15000 });
         
-        message.channel.send('Message author: ' + message.author)
+        msg.channel.send('Message author: ' + msg.author)
         collector.on('collect', m => {
-            message.channel.send('m author: ' + m.author)
-            message.channel.send(`Collected ${m.content}`);
+            msg.channel.send('m author: ' + m.author)
+            msg.channel.send(`Collected ${m.content}`);
             collector.stop()
         });
         
         collector.on('end', collected => {
-            message.channel.send(`Collected ${collected.size} items`);
+            msg.channel.send(`Collected ${collected.size} items`);
         });         
     })
     .then(msg => {
         msg.delete()
     })
-    .catch(err => message.channel.send(err))
+    .catch(err => console.err)
    
 }
 
