522afdacdd315a5b1e33688011dc972bf6f4fcba
Daniel Cahyo <d4niel.cahyo@gmail.com>
2019-11-10 21:03:08 +0800

    :03

--- commands/dict.js
+++ commands/dict.js
@@ -1,21 +1,27 @@
 let dict = (message) => {
     // message.channel.send('Dict called')    
 
-
-    const filter = m => m.author === message.author
-    const collector = message.channel.createMessageCollector(filter, { time: 15000 });
-    
     message.channel.send('Type something..')
-    message.channel.send('Message author: ' + message.author)
-    collector.on('collect', m => {
-        message.channel.send('m author: ' + m.author)
-        message.channel.send(`Collected ${m.content}`);
-        collector.stop()
-    });
-    
-    collector.on('end', collected => {
-        message.channel.send(`Collected ${collected.size} items`);
-    });    
+    .then(msg => {
+        const filter = m => m.author === message.author
+        const collector = message.channel.createMessageCollector(filter, { time: 15000 });
+        
+        message.channel.send('Message author: ' + message.author)
+        collector.on('collect', m => {
+            message.channel.send('m author: ' + m.author)
+            message.channel.send(`Collected ${m.content}`);
+            collector.stop()
+        });
+        
+        collector.on('end', collected => {
+            message.channel.send(`Collected ${collected.size} items`);
+        });         
+    })
+    .then(msg => {
+        msg.delete()
+    })
+    .catch(err => message.channel.send(err))
+   
 }
 
 module.exports = dict
