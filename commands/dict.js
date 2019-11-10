d39e10a6cc7339462e60ce706c459fab38afc326
Daniel Cahyo <d4niel.cahyo@gmail.com>
2019-11-10 20:32:52 +0800

    :32

--- commands/dict.js
+++ commands/dict.js
@@ -8,7 +8,9 @@ let dict = (message) => {
     const collector = message.channel.createMessageCollector(filter, { time: 15000 });
     
     message.channel.send('Type something..')
+    message.channel.send('Message author: ' + message.author)
     collector.on('collect', m => {
+        message.channel.send('m author: ' + m.author)
         message.channel.send(`Collected ${m.content}`);
         collector.stop()
     });
