c309ee9e553c1b7134d64667901c857232d59604
Daniel Cahyo <d4niel.cahyo@gmail.com>
2019-11-10 20:18:48 +0800

    Testing collector

--- commands/dict.js
+++ commands/dict.js
@@ -1,5 +1,17 @@
 let dict = (message) => {
-    message.channel.send('Dict called')    
+    // message.channel.send('Dict called')    
+
+
+    const filter = m => m.content.includes('discord');
+    const collector = message.channel.createMessageCollector(filter, { time: 15000 });
+    
+    collector.on('collect', m => {
+        console.log(`Collected ${m.content}`);
+    });
+    
+    collector.on('end', collected => {
+        console.log(`Collected ${collected.size} items`);
+    });    
 }
 
 module.exports = dict
