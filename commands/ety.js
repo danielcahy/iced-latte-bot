d83a3366e83a472bfcab45108c1a5754c628c6a0
Daniel Cahyo <d4niel.cahyo@gmail.com>
2019-11-11 17:49:59 +0800

    Tries fixing includes

--- commands/ety.js
+++ commands/ety.js
@@ -17,6 +17,7 @@ async function ety(message) {
         result = await new Promise((resolve, reject) => {
             request(requestURL, function (error, response, body) {
                 if (response.status === 200) {
+                  console.log(response)
                   resolve(requestURL)
                 } else {
                   reject(new Error('Character does not exist inside ``chaziwang.com`` database!'))
@@ -28,6 +29,7 @@ async function ety(message) {
     }
 
     //Check if URL or String
+    console.log(result)
     if (result.includes('http')) {
         embed.setImage(result)
     } else {
